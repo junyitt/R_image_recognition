fd54eafa994a072a3d8045a10609708da9ea41fe
junyitt <>
2019-05-30 06:02:21 +0800

    host launch browser

--- __WQD7004_GroupProject/Processes/Deployment_Host.R
+++ __WQD7004_GroupProject/Processes/Deployment_Host.R
@@ -1,6 +1,6 @@
 library(shiny)
 # runApp('./App/FacialRecognitionApp', port = 8000)
-runApp('./App/FacialRecognitionApp', port = 4080, host = "192.168.0.115")
+runApp('./App/FacialRecognitionApp', port = 4080, host = "192.168.0.115", launch.browser = TRUE)
 
 
           
